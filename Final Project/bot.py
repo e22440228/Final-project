906f4455a135576c917c0539443e575eea888370
Chung Ying Lee <e12123484@gmail.com>
2021-11-21 22:16:32 -0500

    Add status.md

--- Final Project/bot.py
+++ Final Project/bot.py
@@ -1,24 +1,41 @@
-#導入 Discord.py
 import discord
-#client 是我們與 Discord 連結的橋樑
-client = discord.Client()
+from discord.ext import commands
+bot = commands.Bot(command_prefix='!')
 
-#調用 event 函式庫
-@client.event
-#當機器人完成啟動時
+@bot.event
 async def on_ready():
-    print('目前登入身份：', client.user)
+    print('>> Bot is online<<')
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(912124958958817361)
+    await channel.send(f'{member} join!')
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(912125467677577277)
+    await channel.send(f'{member} left!')
+@bot.command()
+async def 圖片(ctx):
+    await ctx.send("HI")
+@bot.command()
+async def Games(ctx):
+    await ctx.send('Which one?')
+    await ctx.send('BlackJack, DND')
+    
 
-@client.event
+@bot.event
 #當有訊息時
 async def on_message(message):
     #排除自己的訊息，避免陷入無限循環
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     #如果包含 ping，機器人回傳 pong
     if message.content == 'ping':
         await message.channel.send('pong')
-    if message.content == 'game':
+    if message.content == '!Game':
         await message.channel.send('Which one?')
+        await message.channel.send('BlackJack, DND')
+    if message.content == 'BlackJack':
+        await message.channel.send('Not yet deployment')
 
-client.run('') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
+
+bot.run('***') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
---
+++ status.md
@@ -0,0 +1,34 @@
+# Status Report
+
+#### Your name
+
+Chung Ying Lee
+
+#### Your section leader's name
+
+Don't know
+in case if he/she is TA
+Sai Shi
+
+#### Project title
+
+Game BOT
+
+***
+
+Short answers for the below questions suffice. If you want to alter your plan for your project (and obtain approval for the same), be sure to email your section leader directly!
+
+#### What have you done for your project so far?
+
+Create a discord BOT
+Working on the games
+
+#### What have you not done for your project yet?
+
+create DND
+create BlackJack on pygame
+
+#### What problems, if any, have you encountered?
+
+not too much
+I am still learning how the BOT goes.
